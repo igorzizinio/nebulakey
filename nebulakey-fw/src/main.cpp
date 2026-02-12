c9e06f46edb5f8a6889398b1cdc18e831a3fd91d
igorzizinio <igorschug6@gmail.com>
2026-02-12 10:45:44 -0300

    Initial commit

---
+++ nebulakey-fw/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <ezButton.h>
+
+constexpr const uint8_t PREVIOUS_BUTTON_PIN = 20;
+constexpr const uint8_t NEXT_BUTTON_PIN = 21;
+
+// Encoder pins
+
+// Placeholders, got from https://arduinogetstarted.com/tutorials/arduino-rotary-encoder
+constexpr const uint8_t CLK_PIN = 2;
+constexpr const uint8_t DT_PIN = 3;
+constexpr const uint8_t SW_PIN = 4;
+
+ezButton encoderButton(SW_PIN);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(PREVIOUS_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(NEXT_BUTTON_PIN, INPUT_PULLUP);
+
+  pinMode(CLK_PIN, INPUT);
+  pinMode(DT_PIN, INPUT);
+  encoderButton.setDebounceTime(50);
+}
+
+void loop()
+{
+  encoderButton.loop();
+
+  if (encoderButton.isPressed())
+  {
+    Serial.println("Encoder button pressed (play/pause)");
+  }
+}
